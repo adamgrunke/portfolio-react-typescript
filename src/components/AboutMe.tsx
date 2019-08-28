556adc732fbf92fcaf312699280923da57425fd6
onoroff <adam.grunke@gmail.com>
2019-08-28 13:46:49 -0700

    initial commit

---
+++ src/components/AboutMe.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutMe: React.FC = () => {
+    
+    return (
+        <div>
+            <h1>ACG</h1>
+        </div>
+    );
+    }
+
+    export default AboutMe;
---
+++ src/components/SkillsCollection.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const SkillsCollection: React.FC = () => {
+
+    return(
+        <div>
+            <p>SKILLS <span className="fas fa-camera"></span></p>
+            {/* <i class="fab fa-react"></i> */}
+            
+
+        </div>
+    );
+}
+
+export default SkillsCollection;
