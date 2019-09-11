19ca40f401156bc4ac003f4d4b7440eb8cc38b9e
onoroff <adam.grunke@gmail.com>
2019-09-11 11:43:19 -0700

    tech font awesome icons added

--- src/components/SkillsCollection.tsx
+++ src/components/SkillsCollection.tsx
@@ -7,8 +7,14 @@ const SkillsCollection: React.FC = () => {
     return(
         <div>
             <h2>Technical Skills</h2>
-            <p>Javascript (JS)  |   React (hooks)   |   Typescript (TS)</p> 
-            <p> Express |   CSS |   HTML    |   Embedded Javascript (EJS)   |   Node.js   </p> 
+            <i className="fab fa-react fa-5x"></i>
+            <i className="fab fa-js-square fa-5x"></i>
+            <i className="fab fa-css3 fa-5x"></i>
+            <i className="fab fa-html5 fa-5x"></i>
+            <i className="fab fa-node-js fa-5x"></i>
+            {/* <i className="fab fa-js fa-5x"></i> */}
+            
+            <p> Typescript (TS) |   Express    |   Embedded Javascript (EJS)</p> 
             <p>Django	|   SQL |   Sequelize   |   PostgreSQL  |   Mongoose    |   MongoDB </p>
             <p>Git  |   GitHub  |   Heroku </p>            
         </div>
